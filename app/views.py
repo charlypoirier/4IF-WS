8d6b4a9a9534033f35ab9bac1fff90be79f5c2a3
jerome <hue.jerome@yahoo.fr>
2020-11-04 11:39:34 +0100

    Feat : Passing data to html pages

--- app/views.py
+++ app/views.py
@@ -4,7 +4,8 @@ from flask import render_template
 
 @app.route("/")
 def index():
-    return render_template("index.html")
+    data = {'a': "hello_world 1", 'b': 'hello_world 2','c':'hello world 3'}
+    return render_template("index.html", data=data)
 
 @app.route("/about")
 def about():
