67029ce89989bb550154059086bf079671833fb5
jerome <hue.jerome@yahoo.fr>
2020-11-09 09:31:46 +0100

    New requests examples

--- dbPedia-general.py
+++ dbPedia-general.py
@@ -1,5 +1,7 @@
 from SPARQLWrapper import SPARQLWrapper, JSON
 
+
+# Chercher les auteurs dont le nom contient la chaîne 'Hugo'
 qry = "Hugo"
 rgxqry='\".*' +qry +'.*\"'
 
@@ -24,17 +26,46 @@ results = sparql.query().convert()
 for item in results["results"]["bindings"]:
     print("Auteur : ", item["author"]["value"] )
 
-
+#########################################
+# Sélectionner les auteurs nés en France.
+#########################################
 
 #SELECT ?author WHERE {
- #  ?a a dbo:Writer .
-  # ?a rdfs:label ?author .
-  # ?a dbo:birthPlace dbr:France
+#  ?a a dbo:Writer .
+# ?a rdfs:label ?author .
+# ?a dbo:birthPlace dbr:France
 #}
 
-############################
-# Tous les livres d'un auteur
+##############################
+# Tous les livres d'un auteur.
+##############################
 
 #SELECT * WHERE {
 #        ?book dbo:author <http://dbpedia.org/resource/Mark_Twain>
 #}
+
+
+##################################################
+#Sélectionner les noms des auteurs et leurs livres.
+##################################################
+
+# SELECT * WHERE {
+#    ?author a dbo:Writer .
+#    ?book dbo:author ?author .
+#    ?author rdfs:label  ?label .
+#    FILTER (lang(?label) = 'en')
+#    Filter(regex(?author, ".*Hugo.*"))
+#}
+
+############################################
+# Auteurs appartenant au même genre qu'Hugo. 
+############################################
+
+#SELECT ?romanticAuthor WHERE {
+#    ?author a dbo:Writer ; dbo:movement ?mov .
+#    ?romanticAuthor a dbo:Writer ; dbo:movement ?mov .
+#
+#    Filter(regex(?author, ".*Hugo.*"))
+#}
+
+
