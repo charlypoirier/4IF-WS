a83d4900ac41803351f3d814baf0f9a05c287cfb
Charly Poirier <pro.poiriercharly@gmail.com>
2020-11-10 10:04:15 +0100

    Meilleure lisibilité

--- app/bnf_requests.py
+++ app/bnf_requests.py
@@ -35,21 +35,21 @@ print(namesresults)
 def generic(name):
     sparql = SPARQLWrapper("https://data.bnf.fr/sparql")
     
-    rgxqry='\".*' +name + '.*\"'
+    rgxqry = '".*{0}.*"'.format(name)
     
     sparql.setQuery("""
-    PREFIX bio: <http://vocab.org/bio/0.1/>
-    PREFIX dcterms: <http://purl.org/dc/terms/>
-    PREFIX foaf: <http://xmlns.com/foaf/0.1/>
-    PREFIX xsd: <http://www.w3.org/2001/XMLSchema#>
-    SELECT distinct ?nom ?auteur ?birth
-    WHERE {
-    ?oeuvre dcterms:creator ?auteur.
-    ?auteur bio:birth ?birth ;
-    foaf:name ?nom.
-    FILTER(regex(?nom, """ + rgxqry + """, "i"))
-    }
-    LIMIT 100
+        PREFIX bio: <http://vocab.org/bio/0.1/>
+        PREFIX dcterms: <http://purl.org/dc/terms/>
+        PREFIX foaf: <http://xmlns.com/foaf/0.1/>
+        PREFIX xsd: <http://www.w3.org/2001/XMLSchema#>
+        SELECT distinct ?nom ?auteur ?birth
+        WHERE {
+        ?oeuvre dcterms:creator ?auteur.
+        ?auteur bio:birth ?birth ;
+        foaf:name ?nom.
+        FILTER(regex(?nom, """ + rgxqry + """, "i"))
+        }
+        LIMIT 100
     """)
     sparql.setReturnFormat(JSON)
     results = sparql.query().convert()
